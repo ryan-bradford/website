b5085a30b8adeca0dfecce340994493c67970e01
Ryan Bradford <bradfordr@vmware.com>
2022-08-06 15:30:38 -0400

    Fixed title

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,7 +1,7 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
   homepage: 'https://ryan-bradford.com',
-  title: 'Ryan',
+  title: 'Hello',
 }
 
 const about = {
